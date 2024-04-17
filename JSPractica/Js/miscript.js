128d4b9bae71ae579e630a53bf485205b14c46e3
EmilioPayanOrtega <139089343+EmilioPayanOrtega@users.noreply.github.com>
2024-04-17 10:50:05 -0700

    Primer Practica JavaScript

---
+++ JSPractica/Js/miscript.js
@@ -0,0 +1,16 @@
+var a = 0;
+    let b = 0;
+    const pi = 3.1416;
+    alert("Js en Proceso")
+    function boton(){
+        alert("Clic")
+    }
+
+    /* Funcion anonima
+    let anonima = () {
+        alert("Funcion anonima")
+    }*/
+
+    const flecha = () => {
+        alert("Funcion flecha - anonima")
+    }
