a88d930858516cd4c519f40f6d218cd2355293bf
EmilioPayanOrtega <139089343+EmilioPayanOrtega@users.noreply.github.com>
2024-04-25 11:01:08 -0700

    conexión a mysql

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,25 @@ let mysql = require ('mysql');
 let cors = require ('cors');
 
 let app = express();
-app.use(express.json)
+app.use(express.json())
 app.use(cors());
+
+//Conexion a Mysql 
+let conexion = mysql.createConnection({
+    host: "localhost",
+    user: 'root',
+    password: '',
+    database: 'pwdata',
+})
+
+//Nos conectamos a MySQL
+conexion.connect(function(error){
+    if (error){
+        throw error;
+    }else{
+        console.log('Conectando a la base de datos')
+    }
+});
 
 //Rutas de acceso
 app.get("/",function(req,res){
@@ -12,8 +29,19 @@ app.get("/",function(req,res){
 }
 )
 
+//Seleccionamos todos los clientes
+app.get('/api/clientes',(req,res)=>{
+    conexion.query('SELECT * FROM clientes',(error,filas)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(filas);
+        }
+    });
+})
+
 //Encender el servidor
 let puerto = 3000;
-app.listen(puerto,funtion(){
+app.listen(puerto,function(){
     console.log('Servidor escuchando puerto' + puerto)
 })
