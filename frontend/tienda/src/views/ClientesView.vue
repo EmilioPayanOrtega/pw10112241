602318f2b6f38b96d5966834dffd8961763429e8
EmilioPayanOrtega <139089343+EmilioPayanOrtega@users.noreply.github.com>
2024-05-09 10:58:23 -0700

    Pantalla clientes

---
+++ frontend/tienda/src/views/ClientesView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h4>Clientes
+                    <Routerlink to="/clientes/clientes" class="btn btn-primary float-end">
+                        Agregar
+                    </Routerlink>
+                </h4>
+            </div>
+            <div class="card-body"></div>
+        </div> 
+    </div>         
+    </template>
